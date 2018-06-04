6624fff621c217cbace0b3ec91dcf8dcfa990f8e
吴李辉 <wulihui@datatrees.com.cn>
2018-06-04 14:52:18 +0800

    发包脚本修改

--- pub2dev.sh
+++ pub2dev.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 #开发环境部署脚本
 #cd web/target
-scp main/target/delayqueue-$(date +%F).jar root@192.168.5.22:/dashu/application/bak
+scp main/target/acrm-delayqueue-$(date +%F).jar root@192.168.5.22:/dashu/application/bak
 ssh  root@192.168.5.22<< EOF
     cd /dashu/application;
-    rm -rf delayqueue*.jar;
-    cp bak/delayqueue-$(date +%F).jar delayqueue-$(date +%F).jar
+    rm -rf acrm-delayqueue*.jar;
+    cp bak/acrm-delayqueue-$(date +%F).jar acrm-delayqueue-$(date +%F).jar
     bash restart-delayqueue.sh;
     exit;
 EOF
--- pub2test.sh
+++ pub2test.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 #开发环境部署脚本
 #cd web/target
-scp main/target/delayqueue-$(date +%F).jar root@172.16.1.27:/dashu/application/bak
+scp main/target/acrm-delayqueue-$(date +%F).jar root@172.16.1.27:/dashu/application/bak
 ssh  root@172.16.1.27<< EOF
     cd /dashu/application;
-    rm -rf delayqueue*.jar;
-    cp bak/delayqueue-$(date +%F).jar delayqueue-$(date +%F).jar
+    rm -rf acrm-delayqueue*.jar;
+    cp bak/acrm-delayqueue-$(date +%F).jar acrm-delayqueue-$(date +%F).jar
     bash restart-delayqueue.sh;
     exit;
 EOF
