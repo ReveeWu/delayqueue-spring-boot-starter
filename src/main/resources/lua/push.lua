d1c141a3f7ea8a66262ddbced6b9345db2ff1026
ouyangtao <ouyangtao@treefinance.com.cn>
2018-08-28 19:22:31 +0800

    相同的key，push接口提供3种策略

--- src/main/resources/lua/push.lua
+++ src/main/resources/lua/push.lua
@@ -3,10 +3,21 @@ local metaDataKey = KEYS[2]
 local id = ARGV[1]
 local metaData = ARGV[2]
 local score = ARGV[3]
---如果不存在，保存元数据
-local result = redis.call("hsetnx", metaDataKey, id, metaData)
+local policy = ARGV[4]
+local result = 0
+
+if policy == "IGNORE"
+then
+	--如果不存在，保存元数据
+	result = redis.call("hsetnx", metaDataKey, id, metaData)
+elseif policy == "COVER"
+then
+	--如果存在，覆盖元数据
+	result = redis.call("hset", metaDataKey, id, metaData)
+end
+
 if result==1 then
-   --如果保存元数据成功，则添加zset数据
+	--如果保存元数据成功，则添加zset数据
 	result = redis.call("zadd", zsetKey, score, id)
 end
 return result
