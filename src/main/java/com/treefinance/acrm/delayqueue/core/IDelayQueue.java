9e448d82e4abb5b9b8c7f2ed03dd04df848c844a
ReveeWu <ReveeWu@gmail.com>
2018-10-22 14:53:49 +0800

    异常处理优化 & resume异常处理bug fix

--- src/main/java/com/treefinance/acrm/delayqueue/core/IDelayQueue.java
+++ src/main/java/com/treefinance/acrm/delayqueue/core/IDelayQueue.java
@@ -1,8 +1,5 @@
 package com.treefinance.acrm.delayqueue.core;
 
-import com.treefinance.acrm.delayqueue.core.exception.DataExistsException;
-import com.treefinance.acrm.delayqueue.core.exception.DelayQueueException;
-
 /**
  * @author reveewu
  * @date 07/06/2018
@@ -12,33 +9,34 @@ public interface IDelayQueue {
      * 推送数据到延时队列
      *
      * @param message
-     * @throws DataExistsException
-     * @throws DelayQueueException
+     * @throws Exception
      */
-    void push(DelayMessage message) throws DelayQueueException;
+    void push(DelayMessage message) throws Exception;
 
     /**
      * 根据push策略推送数据到延时队列
      *
      * @param message
      * @param policy
-     * @throws DelayQueueException
+     * @throws Exception
      */
-    void push(DelayMessage message, PolicyEnum policy) throws DelayQueueException;
+    void push(DelayMessage message, PolicyEnum policy) throws Exception;
 
     /**
      * 拉取到期数据
      *
      * @param topic
+     * @throws Exception
      * @return
      */
-    DelayMessageExt pull(String topic) throws DelayQueueException;
+    DelayMessageExt pull(String topic) throws Exception;
 
     /**
      * 消费后回调
      *
      * @param messageExt
      * @param consumeStatus
+     * @throws Exception
      */
-    void callback(DelayMessageExt messageExt, ConsumeStatus consumeStatus);
+    void callback(DelayMessageExt messageExt, ConsumeStatus consumeStatus) throws Exception;
 }
